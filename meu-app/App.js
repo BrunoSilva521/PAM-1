a0ea531dce782b5a96f392f8a6acd3face8974b5
BrunoSilva521 <brunopereirasilva910@gmail.com>
2026-09-18 13:33:30 +0000

    Atividade

--- meu-app/App.js
+++ meu-app/App.js
@@ -4,56 +4,100 @@ import { View, Text, Button, StyleSheet } from 'react-native';
 
 const estilo = StyleSheet.create({
   boxTitulo: {
-    height: 80,
-    backgroundColor: '#1f4f66',
-    paddingHorizontal: 10,
-    paddingTop: 10,
+    height: "10%",
+    width:"200%", 
+    backgroundColor: '#a1e5db',
+    paddingVertical: "10%",
     justifyContent: 'center',
   },
 
   tituloCabecalho: {
-    color: '#0fc3d4',
+    color: '#102a43',
     fontWeight: '700',
-    fontSize: 20,
+    fontSize: "90%",
+    textAlign: "center",
   },
 
   subtitulo: {
-    color: '#fff',
+    color: '#102a43',
+     textAlign: "center",
+     fontSize:"80%",
+
   },
 
   tela: {
     width: '100%',
-    justifyContent: 'center',
+    flex: 1,
+    // justifyContent: "flex-start",
     alignItems: 'center',
+    marginHorizontal: "auto",
+    
+  },
+
+  rodada:{
+  backgroundColor: '#c1ebe4',
+  borderRadius: "10%",
+  height: "3%",
+  width:"50%",
+  marginTop:"10%",
+  marginBottom:"10%",
+  },
+
+  numerosorte:{
+    color: "#797979",
+    fontStyle: "italic",
+  },
+
+  txt:{
+    color: "#102a43",
+    fontWeight: "900",
+  },
+
+  box:{
+   borderColor: "#c4e0dc", 
+   height: 50,
+   borderWidth:2,
+   borderRadius:"%",
+   width: "100%",
+   marginTop: "10%",
+   marginBottom:"10%",
+
   },
 
   boxNumero: {
-    borderColor: '#00a2ff',
-    backgroundColor: '#004869',
-    borderWidth: 5,
-    height: 150,
-    width: 150,
-    borderRadius: 75,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 50,
+    borderColor: '#5be2ce',
+    backgroundColor: '#102a43',
+    borderWidth: 10,
+    height: "20%",
+    width: "75%",
+    borderRadius: "75%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: "10%",
   },
 
   tituloTexto: {
-    fontSize: 14,
-    marginVertical: 30,
-    paddingHorizontal: 20,
-    textAlign: 'center',
+    fontSize: 16,
+    marginVertical: 8,
+    // paddingHorizontal: "20%",
+    textAlign: "center",
+    // alignItems: "center",
+    // margin: "auto",
   },
 
   numero: {
-    fontSize: 80,
+    fontSize: 36,
     color: '#fff',
+    alignItems:"center",
   },
 
   boxBotao: {
-    marginTop: 25,
-    width: 200,
+    marginTop: 8,
+    width: "100%",
+    paddingHorizontal: 10, 
+    alignItems:"center",
+     
+    // margin: "-10%",
   },
 });
 
@@ -127,7 +171,7 @@ const TelaInicial = () => {
   return (
     <View style={estilo.tela}>
 
-      <Text style={estilo.tituloTexto}>
+      <Text style={estilo.rodada}>
        Rodada: {Rodadas} de 5
       </Text>
 
@@ -136,18 +180,19 @@ const TelaInicial = () => {
           {numeroSorteado}
         </Text>
       </View>
-        <Text>
+        <Text style={estilo.numerosorte}>
           Último número sorteado: {UltimoNumeroSorteado}
         </Text>
 
       <View style={estilo.boxBotao}>
-        <Text>
+        <Text style = {estilo.txt}>
         Qual Número será sorteado?
         </Text> 
         <TextInput
           keyboardType='numeric'
           onChangeText={(numero) => setNumeroEscolhido(number(numero))}
-          placeholder='Digite um número de 0 a 99'
+          placeholder='Digite um número de 0 a 99' 
+          style = {estilo.box}
         />
         <Button
           title="Sortear"
